d83a4f3a8bc49c9253a3690d77e7ca2ab6d6b602
Andres Castillo <acastillo119@gmail.com>
2026-02-24 21:27:15 -0500

    Add parent category badget to transaction item

--- budget-ui/src/lib/components/category-badge.svelte
+++ budget-ui/src/lib/components/category-badge.svelte
@@ -7,6 +7,9 @@
 
 	let isIncome = $derived(category.categoryType === 'INCOME');
 	let Icon = $derived(iconMap[category.icon as keyof typeof iconMap]);
+	let ParentIcon = $derived(
+		category.parent ? iconMap[category.parent.icon as keyof typeof iconMap] : null
+	);
 	let colorClass = $derived(
 		isIncome
 			? 'bg-green-100 text-green-800 hover:bg-green-200'
@@ -14,6 +17,12 @@
 	);
 </script>
 
+{#if category.parent && ParentIcon}
+	<Badge variant="outline" class={`${colorClass} ${size === 'sm' ? 'px-1 py-0 text-xs' : ''}`}>
+		<ParentIcon class={`${size === 'sm' ? 'h-3 w-3' : 'h-4 w-4'} mr-1`} />
+		{category.parent.name}
+	</Badge>
+{/if}
 <Badge variant="outline" class={`${colorClass} ${size === 'sm' ? 'px-1 py-0 text-xs' : ''}`}>
 	<Icon class={`${size === 'sm' ? 'h-3 w-3' : 'h-4 w-4'} mr-1`} />
 	{category.name}
